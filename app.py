2c6a69c5f1ddbafdc34550adfcf3a0c3c13ae93e
ilorez <zobirnajdaoui@gmail.com>
2024-03-13 17:32:14 +0000

    upgrade Url obj and rename it to AutoScout24 with get more info from products

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify
-from Scrape import Url
+# from Scrape import Url
+from AutoScout24 import AutoScout24
 app = Flask(__name__)
+
+# generate yield data
+def generate():
+    for i in range(1, 100000000):
+        yield f'Number: {i}\n'
+
+
+
+@app.route('/stream')
+def stream():
+    return generate()
+
 
 @app.route('/')
 def hello():
@@ -8,9 +21,7 @@ def hello():
 
 @app.route('/scrape')
 def scrape():
-    url = Url('https://www.autoscout24.fr/lst/bmw/i3?atype=C&cy=F&damaged_listing=exclude&desc=0&powertype=kw&search_id=majrm8ivsv&sort=standard&source=listpage_pagination&ustate=N%2CU',40,1,30)
+    url = AutoScout24('https://www.autoscout24.fr/lst/bmw/i3?atype=C&cy=F&desc=0&sort=standard&source=homepage_search-mask&ustate=N%2CU',20,6,10)
     return jsonify(url.format_articles_data())
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
